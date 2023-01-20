16e6b5f5481f238dbe1958c0f20f9f5f86e77a13
Rakib-20 <67541363+Rakib-20@users.noreply.github.com>
2023-01-20 18:47:25 +0600

    Update relationalOperatorAnalyzer_with_input.c

--- relationalOperatorAnalyzer_with_input.c
+++ relationalOperatorAnalyzer_with_input.c
@@ -4,22 +4,22 @@
 #include<stdbool.h>
 #include<ctype.h>
 
-void operatorAnalyzer(char* string);        // input string থেকে relational operator analyze করে
-bool isRelaOperator(char c);                    // কোন character relational operator কিনা জানায়
+void operatorAnalyzer(char* string);        
+bool isRelaOperator(char c);                    
 
 
 int main()
 {
-    char string[5000], c = ' ';         // c empty হিসেবে initialize করা
+    char string[5000], c = ' ';         
     printf("Enter a string: ");
     int x = 0;
-    while(c != '\n')                    // c যতক্ষন enter বা newline না পাবে, input নিতে থাকবে
+    while(c != '\n')                    
     {
         scanf("%c", &string[x]);
-        c = string[x];                  // c এর মধ্যে current character store করা
+        c = string[x];                  
         x++;
     }
-    string[x-1] = '\0';                     // input এর শেষে null character দিয়ে string বানিয়ে নেওয়া
+    string[x-1] = '\0';                     
 
     printf("\n\n\n\nOperator Token:\n\n");
 
@@ -33,18 +33,18 @@ void operatorAnalyzer(char* string)
 
     for(int i=0; i<length; i++)
     {
-        if(isRelaOperator(string[i]) == true || string[i] == '=')       // যদি i no. character relational op হয় (<, > etc) বা = হয়
+        if(isRelaOperator(string[i]) == true || string[i] == '=')       
         {
-            if(i+1 < length && string[i+1] == '=')                  // এরসাথেই যদি = sign থাকে, তাহলে এইটা compound operator হয়ে যাবে, ২টা মিলে relational operator
+            if(i+1 < length && string[i+1] == '=')                  
             {
                 printf("%c%c is a valid Relational Operator.\n", string[i], string[i+1]);
                 i++;
             }
-            else if(string[i] == '=')               // শুধু যদি একটা = sign হয়, তাহলে বাদ
+            else if(string[i] == '=')              
                 continue;
-            else if(i+1 < length && (string[i]=='<' && string[i+1]=='<') || (string[i]=='>' && string[i+1]=='>'))      // যদি (<<,>>) হয়, তাহলেও বাদ
+            else if(i+1 < length && (string[i]=='<' && string[i+1]=='<') || (string[i]=='>' && string[i+1]=='>'))      
                 i++;
-            else                                                        // normally এইটা তাহলে relational
+            else                                                        
                 printf("%c is a valid Relational Operator.\n", string[i]);
         }
     }
@@ -56,7 +56,7 @@ bool isRelaOperator(char c)
 
     for(int i=0; i<3; i++)
     {
-        if(c == rela[i])            // relational operator কিনা array-র সাথে মিলিয়ে check করা
+        if(c == rela[i])            
             return true;
     }
     return false;
