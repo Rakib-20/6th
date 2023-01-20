9ddd27e716fca8b985fac7e0ec1615f7aa991df6
Rakib-20 <67541363+Rakib-20@users.noreply.github.com>
2023-01-20 18:38:00 +0600

    Update identifierAnalyzer_with_input.c

--- identifierAnalyzer_with_input.c
+++ identifierAnalyzer_with_input.c
@@ -3,22 +3,22 @@
 #include<stdbool.h>
 #include<ctype.h>
 
-void identifierAnalyzer(char* string);      // input string থেকে identifier operator analyze করে
-bool isKeyword(char* str);                      // কোন string keyword কিনা জানায়
-bool isDelimeter(char c);                           // কোন character delimeter বা separator কিনা জানায়
+void identifierAnalyzer(char* string);      
+bool isKeyword(char* str);                      
+bool isDelimeter(char c);                           
 
 int main()
 {
-    char string[5000], c = ' ';         // c empty হিসেবে initialize করা
+    char string[5000], c = ' ';        
     printf("Enter a string: ");
     int x = 0;
-    while(c != '\n')                        // c যতক্ষন enter বা newline না পাবে, input নিতে থাকবে
+    while(c != '\n')                        
     {
         scanf("%c", &string[x]);
-        c = string[x];                  // c এর মধ্যে current character store করা
+        c = string[x];                  
         x++;
     }
-    string[x-1] = '\0';                 // input এর শেষে null character দিয়ে string বানিয়ে নেওয়া
+    string[x-1] = '\0';                 
 
     identifierAnalyzer(string);
 
@@ -26,41 +26,41 @@ int main()
 
 void identifierAnalyzer(char* string)
 {
-    int left = 0, right = 0;            // token এর শুরু শেষ track রাখে left, right
+    int left = 0, right = 0;            
     int length = strlen(string);
-    char str[500];                      // token গুলাকে store করার জন্য str array
+    char str[500];                      
 
-    while(right < length)               // যতক্ষন না input string শেষ হয়
+    while(right < length)               
     {
-        if(isDelimeter(string[right]) == true)      // যদি current character delimeter হয়, তাহলে এর আগ পর্যন্ত একটা token complete হইছে
+        if(isDelimeter(string[right]) == true)      
         {
-            str[right - left] = '\0';                   // token বা str এর ending বুঝাতে str এর length এর last position-এ null character দেওয়া
-            if(isKeyword(str) == false)                     // str বা token টা যদি keyword না হয়
+            str[right - left] = '\0';                   
+            if(isKeyword(str) == false)                     
             {
-                bool ans = true;                    // ধরে নেই যে token টা একটা identifier, তাই ans হচ্ছে true
+                bool ans = true;                    
 
-                if(isalpha(str[0]) || str[0] == '_')    // 1st character টা যদি কোন alphabet বা underscore(_) হয়, isalpha() built-in function
+                if(isalpha(str[0]) || str[0] == '_')    
                 {
-                    for(int i=1; i<strlen(str); i++)        // এরপর token টার বাকিটুকু check করা
+                    for(int i=1; i<strlen(str); i++)        
                     {
-                        if(isalpha(str[i]) || isdigit(str[i]) || str[i] != '_')     // পরের character গুলা alphabet বা digit বা underscore, যাই হোক সমস্যা নাই
+                        if(isalpha(str[i]) || isdigit(str[i]) || str[i] != '_')     
                             ans = true;
-                        else                                // কিন্তু এগুলা বাদে অন্য কিছু হলেই, এই token টা identifier না, তখন ans = false করে, বের হয়ে যাবে
+                        else                                
                         {
                             ans = false;
                             break;
                         }
                     }
 
-                    if(ans == true)                              // যদি identifier হয়, তাহলে আগে token টা print করা লাগবে
+                    if(ans == true)                              
                         printf("%s is a valid Identifier.\n", str);
                 }
             }
 
-            right++;        // পরের character নিয়ে কাজ করার জন্য right increment হয়
-            left = right;       // next token এর শুরু তাহলে হবে right থেকেই
+            right++;        
+            left = right;       
         }
-        else                                    // যদি delimeter না হয়, তাহলে str token টা বড় হতে থাকবে, character গুলা add হইতে থাকবে token-এ
+        else                                    
         {
             str[right - left] = string[right];
             right++;
@@ -77,7 +77,7 @@ bool isKeyword(char* str)
 
     for (int i = 0; i < 32; ++i)
     {
-        if (strcmp(str,keywords[i]) == 0)       // keyword কিনা array-র সাথে string compare করে check করা
+        if (strcmp(str,keywords[i]) == 0)       
             return true;
     }
 
@@ -89,7 +89,7 @@ bool isDelimeter(char c)
     char delimeter[] ={' ', ',', ';', '(', ')', '\t', '+', '-', '*', '/', '%', '='};
     for(int i=0;i<12;i++)
     {
-        if(c == delimeter[i])       // delimeter কিনা array-র সাথে মিলিয়ে check করা
+        if(c == delimeter[i])     
             return true;
     }
     return false;
