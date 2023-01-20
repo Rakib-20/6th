37bb99f263a0da9cec08e7f5b9d6a860052c5022
Rakib-20 <67541363+Rakib-20@users.noreply.github.com>
2023-01-20 18:23:48 +0600

    Update arithmeticOperatorAnalyzer_with_input.c

--- arithmeticOperatorAnalyzer_with_input.c
+++ arithmeticOperatorAnalyzer_with_input.c
@@ -4,21 +4,21 @@
 #include<stdbool.h>
 #include<ctype.h>
 
-void operatorAnalyzer(char* string);    // input string থেকে arithmetic operator analyze করে
-bool isArithOperator(char c);               // কোন character arithmetic operator কিনা জানায়
+void operatorAnalyzer(char* string);    
+bool isArithOperator(char c);              
 
 int main()
 {
-    char string[5000], c = ' '; // c empty হিসেবে initialize করা
+    char string[5000], c = ' '; 
     printf("Enter a string: ");
     int x = 0;
-    while(c != '\n')            // c যতক্ষন enter বা newline না পাবে, input নিতে থাকবে
+    while(c != '\n')            
     {
         scanf("%c", &string[x]);
-        c = string[x];      // c এর মধ্যে current character store করা
+        c = string[x];      
         x++;
     }
-    string[x-1] = '\0';         // input এর শেষে null character দিয়ে string বানিয়ে নেওয়া
+    string[x-1] = '\0';         
 
     printf("\n\n\n\nOperator Token:\n\n");
 
@@ -32,13 +32,13 @@ void operatorAnalyzer(char* string)
 
     for(int i=0; i<length; i++)
     {
-        if(isArithOperator(string[i]) == true)      // যদি i no. character arithmetic op হয় (+, - etc)
+        if(isArithOperator(string[i]) == true)      
         {
-            if(i+1 < length && string[i+1] == '=') // কিন্তু এরসাথেই যদি = sign থাকে, তাহলে এইটা compound operator হয়ে যাবে, so বাদ
+            if(i+1 < length && string[i+1] == '=') 
                 i++;
-            else if(i+1 < length && (string[i]=='+' && string[i+1]=='+') || (string[i]=='-' && string[i+1]=='-')) // আবার যদি (++, --) হয়, তাহলেও বাদ
+            else if(i+1 < length && (string[i]=='+' && string[i+1]=='+') || (string[i]=='-' && string[i+1]=='-')) 
                 i++;
-            else                                                            // normally এইটা তাহলে arithmetic
+            else                                                            
                 printf("%c is a valid Arithmetic Operator.\n", string[i]);
         }
     }
@@ -50,7 +50,7 @@ bool isArithOperator(char c)
 
     for(int i=0; i<6; i++)
     {
-        if(c == arith[i])       // arithmetic operator কিনা array-র সাথে মিলিয়ে check করা
+        if(c == arith[i])       
             return true;
     }
     return false;
