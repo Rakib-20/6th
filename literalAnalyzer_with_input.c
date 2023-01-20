130d6ea2e88bf5e134af81da8af6e2f3a4abdcdb
Rakib-20 <67541363+Rakib-20@users.noreply.github.com>
2023-01-20 18:41:54 +0600

    Update literalAnalyzer_with_input.c

--- literalAnalyzer_with_input.c
+++ literalAnalyzer_with_input.c
@@ -3,20 +3,20 @@
 #include<stdbool.h>
 #include<ctype.h>
 
-void literalAnalyzer(char* string);     // input string থেকে string literal analyze করে
+void literalAnalyzer(char* string);     
 
 int main()
 {
-    char string[5000], c = ' ';         // c empty হিসেবে initialize করা
+    char string[5000], c = ' ';         
     printf("Enter a string: ");
     int x = 0;
-    while(c != '\n')                    // c যতক্ষন enter বা newline না পাবে, input নিতে থাকবে
+    while(c != '\n')                    
     {
         scanf("%c", &string[x]);
-        c = string[x];                      // c এর মধ্যে current character store করা
+        c = string[x];                      
         x++;
     }
-    string[x-1] = '\0';                 // input এর শেষে null character দিয়ে string বানিয়ে নেওয়া
+    string[x-1] = '\0';                 
 
     printf("\n\n\nString Literal:\n\n");
     literalAnalyzer(string);
@@ -25,39 +25,39 @@ int main()
 
 void literalAnalyzer(char* string)
 {
-    int left = 0, right = 0;        // token এর শুরু শেষ track রাখে left, right
+    int left = 0, right = 0;        
     int length = strlen(string);
-    char str[500];                      // token গুলাকে store করার জন্য str array
+    char str[500];                      
 
     while(right < length)
     {
-        if(string[right] == '"')        // যেই position থেকে quotation mark আসবে
+        if(string[right] == '"')        
         {
             right++;
-            left = right;               // quotation এর পরের position থেকে string token শুরু হবে
-            bool isString = false;          // ধরে নেই যে token টা string না
+            left = right;               
+            bool isString = false;          
 
-            while(right < length)           // input string বাকিটুকু check করা,
+            while(right < length)           
             {
-                if(string[right] == '"')        // যখন আরেকটা quotation আসবে, তখন string true বলা যাবে, আর loop থেকে break করতে হবে
+                if(string[right] == '"')        
                 {
                     isString = true;
                     break;
                 }
 
-                str[right-left] = string[right];        //quotation পাওয়ার আগ পর্যন্ত str token-এ character add করা হবে
+                str[right-left] = string[right];        
                 right++;
 
             }
-            str[right - left] = '\0';               // str token এর ending বুঝাতে শেষে null character দিতে হবে
+            str[right - left] = '\0';               
 
 
-            if(isString == true)                            // string literal হলে print করতে হবে
+            if(isString == true)                            
             {
                 printf("%s is a valid String Literal.\n", str);
             }
 
-            right++;                    // next token নিয়ে কাজ start করা
+            right++;                   
             left = right;
         }
         else
