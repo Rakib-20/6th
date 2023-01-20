8d59372f2ed2a0694e3ab958522e2ef039097401
Rakib-20 <67541363+Rakib-20@users.noreply.github.com>
2023-01-20 18:35:00 +0600

    Update dfaSimulation.c

--- dfaSimulation.c
+++ dfaSimulation.c
@@ -13,51 +13,51 @@ Transitions Are:
 
 #include<stdio.h>
 
-int Move(int s, char c);            // কোনো state থেকে input character এর জন্য next কোন state-এ move করবে সেইটা return করবে
+int Move(int s, char c);            
 
 int main()
-{                                   // main function টা একেবারে doc-file-এর algorithm এর মতোন
+{                                   
     char input[100];
     printf("Provide the input string: ");
     scanf("%s", input);
 
-    int state = 1;                  // state = হচ্ছে starting state = 1
+    int state = 1;                  
     int i = 0;
-    char c = input[i];          // input string থেকে nextchar টা কে c তে store করা
+    char c = input[i];          
 
-    while(input[i] != '\0')         // যতক্ষন পর্যন্ত না null character-এ পৌছে যায়, মানে string শেষ হয়
+    while(input[i] != '\0')         
     {
-        state = Move(state, c);         // current state থেকে input charater এর জন্য নতুন যেই state Move function দিলো, ওইটাই এখন state
+        state = Move(state, c);        
 
-        if(state == -1)                 // যদি কোনো invalid state-এ চলে যায়, তাহলে আর check করার দরকার নাই, এইটা DFA accept করবে না
+        if(state == -1)                 
             break;
         i++;
-        c = input[i];               // c তে nextchar store করা
+        c = input[i];               
     }
 
     printf("Accepted by DFA: ");
 
-    if(state == 3)              // এই DFA-র final state হচ্ছে 3, তাই যদি state এর value 3 হয়, তাহলে YES
+    if(state == 3)              
         printf("YES\n");
 
-    else                    // অন্য কোনো state-এ থাকলে, NO
+    else                    
         printf("NO\n");
 
     return 0;
 }
 
 
-int Move(int s, char c)         // MOVE function টা একেক DFA-র জন্য একেকরকম হবে, DFA অনুযায়ী condition গুলা হবে
+int Move(int s, char c)         
 {
-    if(s == 1)                  // এই DFA-র ৩টা state (1, 2,3), তাই ৩ টা if condition হবে
+    if(s == 1)                  
     {
-        if(c == 'a')            // এই DFA-র ২টা input symbol, তাই প্রত্যেকটা state condition এর মধ্যে ২টা করে if condition হবে input symbol এর জন্য
+        if(c == 'a')            
             return 1;
 
         else if(c == 'b')
             return 2;
 
-        else                // a,b বাদে অন্য কোন input symbol দিলে -1 return করবে, কারন এইটা invalid
+        else                
             return -1;
     }
     else if(s == 2)
